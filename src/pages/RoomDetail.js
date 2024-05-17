05a04c50e9fce22200d6b8418ca16b13598a23c2
Junyho <149153782+Junyho@users.noreply.github.com>
2024-05-17 23:51:06 +0900

    Update RoomDetail.js

--- src/pages/RoomDetail.js
+++ src/pages/RoomDetail.js
@@ -1,70 +1,88 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { format } from 'date-fns';
 
 const RoomDetail = () => {
-  const { roomId } = useParams();
-  const [roomDetail, setRoomDetail] = useState(null);
-  const [roomMembers, setRoomMembers] = useState([]);
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { imageSeed } = location.state || {};
+  const [room, setRoom] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const roomDetailsMock = {
-      1: { 
-        title: '스터디방 1', 
-        majorCategory: '학업', 
-        subCategory: '스터디', 
-        maxPeople: 5, 
-        openTalk: 'http://opentalk.example.com',
-        startDate: '2023-10-01',
-        endDate: '2023-12-31',
-        description: '같이 공부해봐요',
-        members: ['Alice', 'Bob', 'Charlie'],
-        leader: 'Alice',  // 방장 정보 추가
-        image: 'https://source.unsplash.com/random/800x600?study'
-      },
-      2: { 
-        title: '공모전방 1', 
-        majorCategory: '학업', 
-        subCategory: '공모전', 
-        maxPeople: 10, 
-        openTalk: 'http://opentalk.example.com',
-        startDate: '2023-10-10',
-        endDate: '2024-01-10',
-        description: '공모전 열심히 준비해봅시다',
-        members: ['Dave', 'Eve', 'Frank'],
-        leader: 'Dave',  // 방장 정보 추가
-        image: 'https://source.unsplash.com/random/800x600?cooperation'
-      },
+    const fetchRoom = async () => {
+      try {
+        const response = await fetch(`http://localhost:8020/rooms/${id}`);
+        if (!response.ok) {
+          throw new Error('Room not found');
+        }
+        const data = await response.json();
+        console.log("Fetched room data:", data);
+        setRoom(data);
+      } catch (error) {
+        console.error('Failed to fetch room details', error);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    if (roomDetailsMock[roomId]) {
-      setRoomDetail(roomDetailsMock[roomId]);
-      setRoomMembers(roomDetailsMock[roomId].members);
-    }
-  }, [roomId]);
+    fetchRoom();
+  }, [id]);
 
-  if (!roomDetail) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>;
-  }
+  const deleteRoom = async (roomId) => {
+    try {
+      const response = await fetch(`http://localhost:8020/rooms/delete/${roomId}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Failed to delete the room');
+      }
+      return response;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  };
+
+  const formatDate = (dateStr) => format(new Date(dateStr), 'yyyy-MM-dd');
+
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm('정말로 이 방을 삭제하시겠습니까?');
+    if (!confirmDelete) return;
+
+    try {
+      const response = await deleteRoom(id);
+      if (response.ok) {
+        alert('방이 성공적으로 삭제되었습니다.');
+        navigate(`/${room.main_type}`); // 삭제 후 main_type 페이지로 리디렉션
+      } else {
+        alert('방 삭제에 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('Failed to delete room:', error);
+      alert('방 삭제에 실패했습니다.');
+    }
+  };
+
+  if (loading) return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  if (!room) return <p>No room.</p>;
 
   return (
     <div className="flex flex-col min-h-screen text-left">
-
       {/* Navigation Bar */}
       <div className="z-10"><Navbar /></div>
 
       {/* Main Content */}
       <div className="flex flex-row z-5">
-        
         <div className="flex-1 p-4 mt-5">
           <div className="relative">
-            <img src={roomDetail.image} alt={roomDetail.title} className="w-full h-64 object-cover rounded-lg mb-4 blur-sm" />
+            <img src={`https://source.unsplash.com/800x600/?${imageSeed}`} alt={room.project_name} className="w-full h-64 object-cover rounded-lg mb-4 blur-sm" />
             <div className="absolute inset-0 flex flex-col justify-center items-center">
-
-              {/* 폰트 버그 수정 */}
-              <span className="p-2 text-white font-bold text-5xl">{roomDetail.title}</span>
-              <span className="p-2 text-white font-bold text-2xl">{roomDetail.majorCategory} - {roomDetail.subCategory}</span>
+              <span className="text-white text-lg font-bold text-5xl">{room.project_name}</span>
+              <span className="text-white text-lg font-bold text-2xl">{room.main_type} - {room.sub_type}</span>
             </div>
           </div>
 
@@ -73,26 +91,26 @@ const RoomDetail = () => {
               {/* 방 세부사항 */}
               <div className='p-5 justify-center'>
                 <p className='font-bold text-2xl'>방장</p>
-                <p className='text-lg pl-5'>{roomDetail.leader}</p>
+                <p className='text-lg pl-5'>{room.leader}</p>
               </div>
               <div className='p-5 justify-center'>
                 <p className='font-bold text-2xl'>최대 인원수</p>
-                <p className='text-lg pl-5'>{roomDetail.maxPeople}명</p>
+                <p className='text-lg pl-5'>{room.occupancy}명</p>
               </div>
               <div className='p-5 flex flex-row'>
                 <div className='mr-5'>
                   <p className="font-bold text-2xl">시작일</p> 
-                  <p className='text-lg pl-5'>{roomDetail.startDate}</p>
+                  <p className='text-lg pl-5'>{formatDate(room.project_start)}</p>
                 </div>
                 <div className='ml-5'>
                   <p className="font-bold text-2xl">마감일</p> 
-                  <p className='text-lg pl-5'>{roomDetail.endDate}</p>
+                  <p className='text-lg pl-5'>{formatDate(room.project_end)}</p>
                 </div>
               </div>
               <div className='m-5 justify-center'>
                 <p className='font-bold text-2xl'>방 소개 및 방장 프로필</p>
                 <div className="overflow-auto h-48">
-                  <p className="pl-5">{roomDetail.description}</p>
+                  <p className="pl-5">{room.contents}</p>
                 </div>
               </div>
             </div>
@@ -101,15 +119,21 @@ const RoomDetail = () => {
             <div className='p-5 w-1/2 justify-center'>
               <div className='m-5 justify-center'>
                 <p className='font-bold text-2xl'>오픈카톡방 주소</p>
-                <a href={roomDetail.openTalk} className="text-blue-500 hover:text-blue-700 pl-5" target="_blank" rel="noopener noreferrer">{roomDetail.openTalk}</a>
+                <a href={room.kakao_chat} className="text-blue-500 hover:text-blue-700 pl-5" target="_blank" rel="noopener noreferrer">{room.kakao_chat}</a>
               </div>
               <div className='m-5 justify-center'>
-                <h3 className="text-2xl font-bold">참가자</h3>
-                <ul className="list-disc pl-5 mx-5">
-                  {roomMembers.map(member => (
-                    <li key={member}>{member}</li>
-                  ))}
-                </ul>
+                <h2 className="text-xl font-bold">참가자</h2>
+                {room.Users && room.Users.length > 0 ? (
+                  <ul className="list-disc pl-5">
+                    {room.Users.map((user, index) => (
+                      <li key={index} className="my-2">
+                        {`학번: ${user.user_id}  / 이름: ${user.name}  / 전공: ${user.major}`}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p className="text-lg">참여자가 없습니다.</p>
+                )}
               </div>
             </div>
           </div>
@@ -120,8 +144,8 @@ const RoomDetail = () => {
       <div className="flex justify-center my-5">
         <button className="bg-[#42cec8] text-white font-bold py-2 px-4 m-1 rounded">참가</button>
         <button className="bg-[#42cec8] text-white font-bold py-2 px-4 m-1 rounded">탈퇴</button>
-        <button className="bg-[#42cec8] text-white font-bold py-2 px-4 m-1 rounded">수정</button>
-        <button className="bg-[#42cec8] text-white font-bold py-2 px-4 m-1 rounded">삭제</button>
+        <button onClick={() => navigate(`/room_update/${room.id}`, { state: { imageSeed: room.imageSeed } })} className="bg-[#42cec8] text-white font-bold py-2 px-4 m-1 rounded">수정</button>
+        <button onClick={handleDelete} className="bg-[#42cec8] text-white font-bold py-2 px-4 m-1 rounded">삭제</button>
       </div>
 
       <Footer />
